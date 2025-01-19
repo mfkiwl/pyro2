f6db36af51b43e3895d21990fc45be4e221c9cc9
mfkiwl <mfkiwl@users.noreply.github.com>
2025-01-19 10:53:39 +0000

    deploy: a78743ee089c4ceffbb3e14382dfb25c437bb8d8

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,13 +1,12 @@
-var DOCUMENTATION_OPTIONS = {
-    URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '4.0.1.post53+g7b7c6baf',
+const DOCUMENTATION_OPTIONS = {
+    VERSION: '4.0.1.post172+ga78743ee',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
     FILE_SUFFIX: '.html',
     LINK_SUFFIX: '.html',
     HAS_SOURCE: true,
-    SOURCELINK_SUFFIX: '.txt',
+    SOURCELINK_SUFFIX: '',
     NAVIGATION_WITH_KEYS: false,
     SHOW_SEARCH_SUMMARY: true,
     ENABLE_SEARCH_SHORTCUTS: true,
