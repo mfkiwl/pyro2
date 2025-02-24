5476fe394aaeba02373334f8c53422d86d7be09e
mfkiwl <mfkiwl@users.noreply.github.com>
2025-02-24 21:46:18 +0000

    deploy: ee3b846116270e82715e23a3493dab6168cab0e4

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,5 +1,5 @@
 const DOCUMENTATION_OPTIONS = {
-    VERSION: '4.0.1.post172+ga78743ee',
+    VERSION: '4.0.1.post196+gee3b8461',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
